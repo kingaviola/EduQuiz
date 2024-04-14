caa16834770c46a45d3a7e4cbdd43d2dc7523b91
kinga.viola <kinga.viola@gmail.com>
2024-04-14 15:29:16 +0200

    Find pie chart example and test it.

--- Frontend/edu-quiz/src/app/pages/statistics/statistics.component.ts
+++ Frontend/edu-quiz/src/app/pages/statistics/statistics.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, ScaleType } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-statistics',
@@ -50,7 +51,7 @@ export class StatisticsComponent {
     }
   ];
 
-  view: [number, number] = [700, 400];
+  view: [number, number] = [700, 150];
 
   // options
   showXAxis: boolean = true;
@@ -58,20 +59,64 @@ export class StatisticsComponent {
   gradient: boolean = false;
   showLegend: boolean = true;
   showXAxisLabel: boolean = true;
-  xAxisLabel: string = 'Country';
+  xAxisLabel: string = 'Kitöltések száma';
   showYAxisLabel: boolean = true;
-  yAxisLabel: string = 'Population';
+  yAxisLabel: string = 'Kérdések';
   animations: boolean = true;
 
   colorScheme = {
     domain: ['#5AA454', '#C7B42C', '#AAAAAA']
   };
 
-  constructor() {
-    //Object.assign(this, { multi });
-  }
+  constructor() { }
 
   onSelect(event: any) {
     console.log(event);
+  }
+
+  single = [
+    {
+      "name": "Always",
+      "value": 8940000
+    },
+    {
+      "name": "Often",
+      "value": 5000000
+    },
+    {
+      "name": "Rarely",
+      "value": 7200000
+    },
+      {
+      "name": "Never",
+      "value": 6200000
+    }
+  ];
+
+  // options
+  gradient2: boolean = true;
+  showLegend2: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+
+  myColor: Color = {
+    name: 'myColors',
+    selectable: true,
+    group: ScaleType.Ordinal,
+    domain: ['#2596be', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  view2: [number, number] = [900, 400];
+
+  onSelect2(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 }
